2272b9523c49ed31cb00546a8d97d91eee204fb0
micsoc <michal.socha.88@gmail.com>
2020-02-25 01:32:10 +0100

    all autoincrements = identity

--- src/main/java/pl/micsoc/dictionary/model/Role.java
+++ src/main/java/pl/micsoc/dictionary/model/Role.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 @Table(name = "roles")
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
     private int id;
     @Column(name = "role")
