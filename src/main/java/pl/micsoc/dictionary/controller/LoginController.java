3795125f55161a8b3716ac12a9a7a8005386042f
Michal Socha <michal.socha.88@gmail.com>
2020-04-07 20:45:34 +0200

    login form upgrade

--- src/main/java/pl/micsoc/dictionary/controller/LoginController.java
+++ src/main/java/pl/micsoc/dictionary/controller/LoginController.java
@@ -44,23 +44,28 @@ public class LoginController {
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
-        User userExists = userService.findUserByUserName(user.getUserName());
-        if (userExists == null) {
-            userExists = userService.findUserByEmail(user.getEmail());
-        }
+        User userExists = userService.findUserByEmail(user.getEmail());
         if (userExists != null) {
             bindingResult
+                    .rejectValue("email", "error.user",
+                            "Użytkownik o podanym adresie email.");
+            userExists = null;
+        }
+
+        if (userExists == null) {
+            userExists = userService.findUserByUserName(user.getUserName());
+        } if (userExists != null) {
+            bindingResult
                     .rejectValue("userName", "error.user",
-                            "There is already a user registered with the user name or email provided");
+                            "Użytkownik o podanej nazwie już istnieje.");
         }
         if (bindingResult.hasErrors()) {
             modelAndView.setViewName("registration");
         } else {
             userService.saveUser(user);
-            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("successMessage", "Pomyślnie zarejestrowano użytkownika");
             modelAndView.addObject("user", new User());
             modelAndView.setViewName("registration");
-
         }
         return modelAndView;
     }
--- src/main/java/pl/micsoc/dictionary/model/User.java
+++ src/main/java/pl/micsoc/dictionary/model/User.java
@@ -26,22 +26,22 @@ public class User {
     private int id;
 
     @Column(name = "user_name")
-    @Length(min = 5, message = "*Your user name must have at least 5 characters")
-    @NotEmpty(message = "*Please provide a user name")
+    @Length(min = 3, message = "*Nazwa musi składać się z conajmniej 3 znaków")
+    @NotEmpty(message = "*Proszę podać nazwę użytkownika")
     private String userName;
 
     @Column(name = "email")
-    @Email(message = "*Please provide a valid Email")
+    @Email(message = "*Proszę podać poprawny adres email")
 //    @NotEmpty(message = "*Please provide an email")
     private String email;
 
     @Column(name = "password")
-    @Length(min = 5, message = "*Your password must have at least 5 characters")
-    @NotEmpty(message = "*Please provide your password")
+    @Length(min = 5, message = "*Hasło musi skłądać się z conajmniej 5 znaków")
+    @NotEmpty(message = "*Proszę wpisać hasło")
     private String password;
 
     @Column(name = "name")
-    @NotEmpty(message = "*Please provide your name")
+//    @NotEmpty(message = "*Please provide your name")
     private String name;
 
     @Column(name = "active")
